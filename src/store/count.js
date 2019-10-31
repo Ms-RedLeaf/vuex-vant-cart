1daa864f863b2f1cd59024bc241c777f3c4ee363
xueqinngchenn <chenqenn@foxmail.com>
2019-10-31 22:52:17 +0800

    从vuex模块化变为正常

--- src/store/count.js
+++ src/store/count.js
@@ -1,21 +1,4 @@
 export default {
   namespaced: true,
-  state: {
-    count: 1,
-  },
-  mutations: {
-    plus(state) {
-      state.count += 1;
-    },
-    reduce(state) {
-      state.count -= 1;
-    },
-  },
-  actions: {
-    asyncPlus({ commit }) {
-      setTimeout(() => {
-        commit('plus');
-      }, 1000);
-    },
-  },
+  
 };
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,24 @@ import count from './count';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  state: {
+    count: 1,
+  },
+  mutations: {
+    plus(state) {
+      state.count += 1;
+    },
+    reduce(state) {
+      state.count -= 1;
+    },
+  },
+  actions: {
+    asyncPlus({ commit }) {
+      setTimeout(() => {
+        commit('plus');
+      }, 1000);
+    },
+  },
   modules: {
     count,
   },
